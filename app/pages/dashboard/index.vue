5da8c43c24545f4998fb5c816cf418c745ae5b99
elahevakili <147205901+elahevakili@users.noreply.github.com>
2025-11-22 19:10:37 +0330

    Add ProjectList component

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -55,8 +55,8 @@ import TeamCollaboration from '~/components/dashboard/TeamCollaboration.vue';
 import ProjectAnalytics from '~/components/dashboard/ProjectAnalytics.vue';
 import ProgressCard from '~/components/dashboard/ProgressCard.vue';
 import ProjectProgress from '~/components/dashboard/ProjectProgress.vue';
-import ProjectsList from '~/components/dashboard/ProjectsList.vue';
 import TimeTracker from '~/components/dashboard/TimeTracker.vue';
+import ProjectsList from '~/components/dashboard/ProjectsList.vue';
 
 
 definePageMeta({
